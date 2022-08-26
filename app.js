9ce573bdb14c50a4b8ac2d0b998dfa66cc4c251b
jjzslsk <3702740@qq.com>
2022-08-26 14:57:16 +0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 const port = 3000
-// mongoose.connect('mongodb://jjzslsk.gicp.net:40203/users');
 mongoose.connect('mongodb://localhost:27017/users');
 
 var Schema = mongoose.Schema;
@@ -81,32 +80,98 @@ app.get('/userLogin', (req, resp) =>{
         }
     })
 })
+app.get('/delUser',(req,resp)=>{
+    User.remove({
+            _id: req.query.id
+        },(err,res)=>{
+            if(err){
+                console.log("删除失败",res);
+                const param = {
+                    code: 0,
+                    message: "删除失败",
+                    success: false
+                }
+                resp.send(param)
+            }
+            else {
+                console.log("删除成功，res是",res)
+                const param = {
+                    code: 0,
+                    message: "删除成功",
+                    success: true
+                }
+                resp.send(param)
+            };
+        }
+    )
+})
+app.get('/editUser',(req,resp)=>{
+    User.findByIdAndUpdate(req.query.id,{
+        user:'lsk'
+    },(err,res)=>{
+        if(err){
+            console.log("修改失败",res);
+            const param = {
+                code: 0,
+                message: "修改失败",
+                success: false
+            }
+            resp.send(param)
+        }
+        else {
+            console.log("修改成功，res是",res)
+            const param = {
+                code: 0,
+                message: "修改成功",
+                success: true
+            }
+            resp.send(param)
+        };
+    })
+})
 app.get('/addUser', (req, resp) =>{
     const param = new User({
         user:req.query.username,
         paw:req.query.password,
     })
-    param.save(function(err,res){
+    User.find({user:req.query.username},function(err,ret){
         if(err){
-            console.log('保存失败')
-            const param = {
-                code: 1,
-                data: res,
-                message: "登录失败",
-                success: false
-            }
-            resp.send(param)
+            console.log('查询失败',err)
         }else{
-            console.log('保存成功')
-            const param = {
-                code: 0,
-                data: res,
-                message: "登录成功",
-                success: true
+            if(ret.length > 0){
+                console.log('查询成功',ret)
+                const param = {
+                    code: 200,
+                    message: "用户已存在",
+                    success: false
+                }
+                resp.send(param)
+            }else{
+                param.save(function(err,res){
+                    if(err){
+                        console.log('保存失败')
+                        const param = {
+                            code: 1,
+                            data: res,
+                            message: "保存失败",
+                            success: false
+                        }
+                        resp.send(param)
+                    }else{
+                        console.log('保存成功')
+                        const param = {
+                            code: 0,
+                            data: res,
+                            message: "保存成功",
+                            success: true
+                        }
+                        resp.send(param)
+                    }
+                })
             }
-            resp.send(param)
         }
     })
+    
 })
 app.get('/login.html', (req, res) =>{
     res.render('login.html', {
--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,46 @@ if(localStorage.getItem('token') && localStorage.getItem('token').length > 0){
   let baseUrl = window.location.protocol + "//" + window.location.host;
   window.location.href = baseUrl.substring(0, baseUrl.length-4) + "3000/login.html";
 }
+function logoutFn(){
+    localStorage.removeItem('token')
+    let baseUrl = window.location.protocol + "//" + window.location.host;
+    window.location.href = baseUrl.substring(0, baseUrl.length-4) + "3000/login.html";
+}
+function edit(id){
+  id = id.replace(/"/g,"")
+  $.ajax({
+      url:`/editUser?id=${id}`,
+      type:'get',
+      success:function (result) {
+          console.log(JSON.stringify(result))
+          if(result.success){
+          let baseUrl = window.location.protocol + "//" + window.location.host;
+          window.location.href = baseUrl.substring(0, baseUrl.length-4) + "3000/index.html";
+          }else{
+            alert(result.message);
+          }
+      },
+      error:function (msg) {
+          alert('系统发生错误');
+      }
+  })
+}
+function del(id){
+  id = id.replace(/"/g,"")
+  $.ajax({
+      url:`/delUser?id=${id}`,
+      type:'get',
+      success:function (result) {
+          console.log(JSON.stringify(result))
+          if(result.success){
+          let baseUrl = window.location.protocol + "//" + window.location.host;
+          window.location.href = baseUrl.substring(0, baseUrl.length-4) + "3000/index.html";
+          }else{
+            alert(result.message);
+          }
+      },
+      error:function (msg) {
+          alert('系统发生错误');
+      }
+  })
+}
